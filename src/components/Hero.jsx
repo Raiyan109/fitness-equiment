857e69bcee7b67c76e0f4276ab5ca1b09f57d309
Raiyan109 <kraiyan109@gmail.com>
2024-07-17 15:28:27 +0600

    hero and navbar done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,49 @@
 import { FaStarOfLife } from "react-icons/fa";
 import { CgGym } from "react-icons/cg";
+import burble from '../assets/burble.jpg'
+import punchBag from '../assets/punch-bag.jpg'
 const Hero = () => {
     return (
-        <div className="bg-gray mx-20 my-7 rounded-2xl py-7 px-16">
-            <h3 className="pb-6">Make your body fit this spring with us.</h3>
+        <div className="bg-gray mx-5 lg:mx-20 my-2 lg:my-2 rounded-2xl py-7 px-6 lg:px-16">
+            <h4 className="pb-3">Make your body fit this spring with us.</h4>
             <div className="border-b "></div>
-            <div className="flex items-end gap-80">
-                <div>
-                    <h1 className="inline-flex gap-7 items-center mt-5">Improve your <span className=""><CgGym className="text-orange border border-orange border-dashed rounded-2xl font-bold p-1" size={70} /></span></h1>
+            <div className="flex gap-8 flex-col md:flex-row">
+                <div className="flex-2 ">
+                    <h1 className="inline-flex gap-7 items-center mt-5">Improve your
+                        <span className="">
+                            <CgGym className="text-orange border border-orange border-dashed rounded-2xl font-bold p-1 text-[50px] lg:text-[80px]" />
+                        </span>
+                    </h1>
 
-                    <h1 className=""> fitness level for</h1>
-                    <h1 className=" inline-flex items-center gap-6">the better <h5 className="capitalize max-w-[200px] font-semibold tracking-tighter">More Than 1900 Fitness Group Classes per month, find new friends who will motivate your fitness goals</h5></h1>
+                    <h1 className="">fitness level for</h1>
+                    <h1 className=" inline-flex items-center gap-6 mb-5">the better
+                        <h5 className="capitalize max-w-[150px] lg:max-w-[200px] font-semibold tracking-tighter leading-3 lg:leading-none">More Than 1900 Fitness Group Classes per month, find new friends who will motivate your fitness goals
+                        </h5>
+                    </h1>
+                    <div>
+                        <button className="btn-orange">Free Pass</button>
+                    </div>
                 </div>
-                <div className="flex flex-col gap-12">
-                    <FaStarOfLife className="text-orange" size={180} />
-                    <button className="btn-orange">Free pass</button>
+                <div className="bg-black rounded-2xl flex-1 mt-6 border border-8 relative">
+                    <img src={burble} alt="" className="rounded-2xl  object-cover object-center " />
+                    <div className="absolute bottom-10 left-12">
+                        <h2 className="text-gray/80 opacity-40 leading-snug">Lets try</h2>
+                        <h2 className="text-white leading-snug">Reach our body goals</h2>
+                        <h2 className="text-gray opacity-75 leading-snug">body goals target</h2>
+                    </div>
                 </div>
             </div>
 
-
+            {/* <div className="grid grid-cols-3 gap-5">
+                <div className="col-span-1 flex flex-col gap-3 bg-black rounded-2xl">
+                    <div className="bg-black rounded-2xl w-full">
+                        <img src={burble} alt="" className="rounded-2xl  object-cover object-center h-96 w-96" />
+                    </div>
+                </div>
+                <div className="col-span-2 bg-blue rounded-2xl w-full">
+                    <img src={punchBag} alt="" className="rounded-2xl  object-cover object-center h-96 w-96" />
+                </div>
+            </div> */}
 
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
     const activeLink = 'text-primary font-bold flex items-center px-4 -mb-1 text-xl'
     return (
         <div>
-            <header className="p-4 bg-gray mx-20 my-7 rounded-full">
-                <div className="flex justify-between h-8 lg:h-14 py-2 lg:py-3 px-2 lg:px-10">
+            <header className="p-4 bg-gray mx-5 lg:mx-20 my-4 lg:my-4 rounded-full">
+                <div className="flex justify-between h-8 lg:h-8 py-2 lg:py-3 px-2 lg:px-10">
                     <div className="flex gap-10">
                         <Link to='/' className="flex items-center p-2 font-anton text-xl">
                             <h3>RHINOS GYM</h3>
@@ -51,8 +51,8 @@ const Navbar = () => {
                         </ul>
                     </div>
 
-                    <div className="flex justify-center items-center">
-                        <button className="p-4 lg:hidden" onClick={toggleMobileMenu}>
+                    <div className="flex justify-center items-center lg:hidden">
+                        <button className="p-4 " onClick={toggleMobileMenu}>
                             <BiMenuAltRight size={24} />
                         </button>
                     </div>
@@ -65,33 +65,33 @@ const Navbar = () => {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
                         duration: 0.5,
-                        delay: 0.1,
+                        delay: 0.0,
                         ease: [0, 0.71, 0.2, 1.01]
                     }}
-                    className="lg:hidden bg-gray m-7 rounded-t-2xl rounded-b-[50px] py-5">
+                    className="lg:hidden bg-gray m-7 rounded-t-2xl rounded-b-[50px] py-5 absolute  w-[450px] md:w-[870px] top-16 inset-0 bg-opacity-60 backdrop-blur-sm z-10">
                     <ul className="flex flex-col items-center space-y-3 mt-4">
                         <li className="flex">
-                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent relative text-[17px]">
+                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent relative text-5xl leading-snug hover:underline">
                                 Home
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-[17px]">
+                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-5xl leading-snug hover:underline">
                                 Goals
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-[17px]">
+                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-5xl leading-snug hover:underline">
                                 Services
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-[17px]">
+                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-5xl leading-snug hover:underline">
                                 Class Schedules
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-[17px]">
+                            <Link to='/' className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-primary border-blue-600 text-5xl leading-snug hover:underline">
                                 Contact
                             </Link>
                         </li>
